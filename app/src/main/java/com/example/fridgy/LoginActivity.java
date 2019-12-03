a95589d84ff3f5b7f9f9bbeb060aeaca632211a7
Bukitei <borjagomezb@gmail.com>
2019-12-04 00:25:33 +0100

    login...

--- app/src/main/java/com/example/fridgy/LoginActivity.java
+++ app/src/main/java/com/example/fridgy/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +13,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +30,7 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText edtUser, edtPasswd;
     Button bttnLogin;
+    String URL_LOGIN = "https://fridgy.000webhostapp.com/userConnect.php";
 
 
     @Override
@@ -37,33 +44,56 @@ public class LoginActivity extends AppCompatActivity {
         bttnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                validarUsuario("https://fridgy.000webhostapp.com/userConnect.php");
+                String user = edtUser.getText().toString().trim();
+                String pass = edtPasswd.getText().toString().trim();
+
+                if(!user.isEmpty() || !pass.isEmpty()){
+                    Login(user, pass);
+                }else{
+                    makeText(LoginActivity.this, "Introduce todos los campos para continuar", LENGTH_SHORT);
+                }
             }
         });
     }
 
-    private void validarUsuario(String URL){
-        StringRequest stringRequest= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if(!response.isEmpty()){
-                    makeText(LoginActivity.this, "Conseguistes entrar", LENGTH_SHORT);
-                }else{
-                    makeText(LoginActivity.this, "Usuario o contraseña incorrecto", LENGTH_SHORT);
-                }
-            }
-        }, new Response.ErrorListener() {
+    private void Login(final String email, final String password){
+
+        bttnLogin.setVisibility(View.GONE);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String id_user = jsonObject.getString("id_user");
+                        String name = jsonObject.getString("name");
+
+                        if(!id_user.isEmpty()){
+
+                            Toast.makeText(LoginActivity.this, "Has conseguido loguearte con exito"+name, LENGTH_SHORT).show();
+
+                        }else{
+                            makeText(LoginActivity.this, "No existe el usuario", LENGTH_SHORT);
+                        }
+                        }catch(JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(LoginActivity.this, "Error", LENGTH_SHORT).show();
+                            }
+                    }
+                }, new Response.ErrorListener(){
             @Override
             public void onErrorResponse(VolleyError error) {
-                makeText(LoginActivity.this, error.toString(), LENGTH_SHORT);
+                Toast.makeText(LoginActivity.this, "Error "+error, LENGTH_SHORT).show();
             }
-        }){
+        })
+        {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> paramMap = new HashMap<>();
-                paramMap.put("user", edtUser.getText().toString());
-                paramMap.put("password", edtPasswd.getText().toString());
-                return paramMap;
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                return super.getParams();
             }
         };
 
