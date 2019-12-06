7122e97a997ad48cbab58ab5f17a2cdf66c8f2ef
Bukitei <borjagomezb@gmail.com>
2019-12-06 01:02:43 +0100

    hello fellows

--- app/src/main/java/com/example/fridgy/LoginActivity.java
+++ app/src/main/java/com/example/fridgy/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.fridgy;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +16,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -22,8 +26,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import static android.widget.Toast.*;
 
@@ -33,10 +46,10 @@ public class LoginActivity extends AppCompatActivity {
     Button bttnLogin;
     String URL_LOGIN = "https://fridgy.000webhostapp.com/userConnect.php";
 
-    Intent intent = new Intent(this, MenuActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        handleSSLHandshake();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtUser = findViewById(R.id.edtUser);
@@ -62,37 +75,38 @@ public class LoginActivity extends AppCompatActivity {
 
         bttnLogin.setVisibility(View.GONE);
 
-
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
                 new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response) {
                         try {
-                        JSONObject jsonObject = new JSONObject(response);
-                            JSONArray array = new JSONArray(jsonObject.getJSONArray("user"));
-                            JSONObject user = array.getJSONObject(0);
-                        String name = user.getString("user");
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray object = new JSONArray(jsonObject.getString("user"));
+                            JSONObject definitive = object.getJSONObject(0);
+                            String name = definitive.getString("user");
 
-                        if(!name.isEmpty()){
+                            if(!name.isEmpty()){
 
-                            Toast.makeText(LoginActivity.this, "Has conseguido loguearte con exito"+name, LENGTH_SHORT).show();
+                                Toast.makeText(LoginActivity.this, "Has conseguido loguearte con exito: "+name, LENGTH_SHORT).show();
+                                bttnLogin.setVisibility(View.VISIBLE);
+                                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                                startActivity(intent);
 
-                            startActivity(intent);
-
-
-                        }else{
-                            makeText(LoginActivity.this, "No existe el usuario", LENGTH_SHORT);
-                        }
-                        }catch(JSONException e){
-                            e.printStackTrace();
-                            Toast.makeText(LoginActivity.this, "Error", LENGTH_SHORT).show();
-                            startActivity(intent);
+                            }else{
+                                makeText(LoginActivity.this, "No existe el usuario", LENGTH_SHORT);
+                                bttnLogin.setVisibility(View.VISIBLE);
                             }
+                        }catch(JSONException e){
+                            Log.e("error2", e.toString());
+                            Toast.makeText(LoginActivity.this, "Error", LENGTH_SHORT).show();
+                            bttnLogin.setVisibility(View.VISIBLE);
+                        }
                     }
                 }, new Response.ErrorListener(){
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(LoginActivity.this, "Error "+error, LENGTH_SHORT).show();
+                Log.e("error1","Error"+error.toString());
+                bttnLogin.setVisibility(View.VISIBLE);
             }
         })
         {
@@ -109,6 +123,9 @@ public class LoginActivity extends AppCompatActivity {
         requestQueue.add(stringRequest);
     }
 
+    /**
+     * Enables https connections
+     */
     @SuppressLint("TrulyRandom")
     public static void handleSSLHandshake() {
         try {
